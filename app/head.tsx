525cdf401390a9ce84b7eace64d72806a45da79d
Mladen Todorovic <tm.djomla79@gmail.com>
2025-06-28 14:13:49 +0200

    Fix head.tsx file

--- app/head.tsx
+++ app/head.tsx
@@ -1,12 +1,13 @@
-const Head = () => {
+export default function Head() {
   return (
     <>
+      <title>Mladen Todorović | Portfolio</title>
+      <meta charSet='UTF-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta
         name='google-site-verification'
         content='H_53LpgYC75gy-aYmqhM43C0xgUCxptcEDHKdwkLcUY'
       />
     </>
   );
-};
-
-export default Head;
+}
