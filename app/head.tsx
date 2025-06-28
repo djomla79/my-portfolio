f0efcdb45edea2f55c99de2152ed86fef635a5a1
Mladen Todorovic <tm.djomla79@gmail.com>
2025-06-28 13:50:42 +0200

    Add head.tsx file with seo  google verification

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+const Head = () => {
+  return (
+    <>
+      <meta
+        name='google-site-verification'
+        content='H_53LpgYC75gy-aYmqhM43C0xgUCxptcEDHKdwkLcUY'
+      />
+    </>
+  );
+};
+
+export default Head;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
-import ClientLayout from '@/components/layout/ClientLayout';
+'use client';
 
-export const metadata = {
-  verification: {
-    google: 'H_53LpgYC75gy-aYmqhM43C0xgUCxptcEDHKdwkLcUY',
-  },
-};
+import Providers from '../components/providers/Providers';
+import Navbar from '../components/navigation/Navbar';
+
+import './globals.css';
 
 export default function RootLayout({
   children,
@@ -14,7 +13,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className='bg-gray-50 text-gray-900'>
-        <ClientLayout>{children}</ClientLayout>
+        <Providers>
+          <Navbar />
+          <main className='container mx-auto px-4 py-8'>{children}</main>
+        </Providers>
       </body>
     </html>
   );
